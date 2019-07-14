14460f7f3d063fb6ea96824983201e7c13a634c9
Luiz G. Galvão <llgalvao24@gmail.com>
2019-07-14 16:40:13 -0300

    Criacao interface ComentarioRepository

---
+++ src/main/java/br/com/helpets/helpetsapi/comentario/ComentarioRepository.java
@@ -0,0 +1,9 @@
+package br.com.helpets.helpetsapi.comentario;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
+}
