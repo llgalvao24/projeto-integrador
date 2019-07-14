a57e0ef1d63a1d9f4cdf1e3abfda9f314993af84
FillipiSilva <fillipi.silva.fs@gmail.com>
2019-07-14 20:53:33 -0300

    Adicionado CRUD do perfil

---
+++ src/main/java/br/com/helpets/helpetsapi/perfil/PerfilRepository.java
@@ -0,0 +1,20 @@
+package br.com.helpets.helpetsapi.perfil;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface PerfilRepository extends JpaRepository<Perfil,Long> {
+
+    List<Perfil> findByName(String nome);
+
+    @Modifying
+    @Query(value = "update Perfil u set u.nome = :nome where u.id = :id")
+    void updateNome(@Param("nome") String nome, @Param("id") Long id);
+
+    void delete(Long id);
+}
