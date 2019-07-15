b62f82e53d424c23d79506af9db371943a16113c
Leticia <leticiadepadua7@gmail.com>
2019-07-15 15:47:50 -0300

    Criado entidade padrinho

--- src/main/java/br/com/helpets/helpetsapi/padrinho/Padrinho.java
+++ src/main/java/br/com/helpets/helpetsapi/padrinho/Padrinho.java
@@ -1,4 +1,37 @@
 package br.com.helpets.helpetsapi.padrinho;
 
+import br.com.helpets.helpetsapi.perfil.Perfil;
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Data
+
+@Entity
 public class Padrinho {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    private Integer idPadrinho;
+
+    @OneToOne
+    @JoinColumn
+    private Perfil perfil;
+
+    @NotNull
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String senha;
+
+    @NotNull
+    private String image;
+// me disseram que pra imagem é preciso colocar o link dela.
+
+    @NotNull
+    private Integer frequencia;
+
 }
