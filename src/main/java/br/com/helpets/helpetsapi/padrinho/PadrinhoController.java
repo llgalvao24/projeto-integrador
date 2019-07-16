0f8243ef1b93ce297766c00497a379826ecd75e7
Leticia <leticiadepadua7@gmail.com>
2019-07-16 17:27:53 -0300

    Estruturando crud

--- src/main/java/br/com/helpets/helpetsapi/padrinho/PadrinhoController.java
+++ src/main/java/br/com/helpets/helpetsapi/padrinho/PadrinhoController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/v1")
 public class PadrinhoController {
@@ -11,9 +14,39 @@ public class PadrinhoController {
     @Autowired
     private PadrinhoRepository padrinhoRepository;
 
+    //CREATE
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/padrinho")
     public Padrinho save(@RequestBody Padrinho padrinho) {
         return padrinhoRepository.save(padrinho);
     }
+
+    //READ
+    @GetMapping("/padrinho")
+    public List<Padrinho> findAll() {
+        return padrinhoRepository.findAll();
+    }
+
+    @GetMapping("/padrinho/{id}")
+    public Optional<Padrinho> findById(@PathVariable Long id) {
+        return padrinhoRepository.findById(id);
+    }
+
+    @PostMapping("/padrinho/nome")
+    public Optional<Padrinho> findByName(@RequestParam String nome) {
+        return padrinhoRepository.findbyName(nome);
+    }
+
+    //UPDATE
+    @PatchMapping("/padrinho/atualiza/{id}")
+    public void updateNome(@PathVariable Long id, @RequestParam String nome) {
+        padrinhoRepository.updateNome(nome, id);
+    }
+
+    // DELETE
+    @DeleteMapping("/padrinho/{id}")
+    public void delete(@PathVariable Long id) {
+        padrinhoRepository.delete(id);
+    }
+
 }
--- src/main/java/br/com/helpets/helpetsapi/padrinho/PadrinhoRepository.java
+++ src/main/java/br/com/helpets/helpetsapi/padrinho/PadrinhoRepository.java
@@ -1,6 +1,22 @@
 package br.com.helpets.helpetsapi.padrinho;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface PadrinhoRepository extends JpaRepository<Padrinho, Long> {
+
+
+    Optional<Padrinho> findbyName(String nome);
+
+    @Modifying
+    @Query(value = "update Padrinho u set u.nome = :where u.id = :id")
+    void updateNome(@Param("nome") String nome, @Param("id") Long id);
+
+    default void delete(Long id) {
+
+    }
 }
