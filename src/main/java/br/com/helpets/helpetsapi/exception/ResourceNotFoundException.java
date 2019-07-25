13de7f51fe1d2304e62efae099b6dad8d0ace04b
Luiz G. Galvão <llgalvao24@gmail.com>
2019-07-25 14:58:17 -0300

    Adicionado classes no pacote exception

--- src/main/java/br/com/helpets/helpetsapi/exception/ResourceNotFoundException.java
+++ src/main/java/br/com/helpets/helpetsapi/exception/ResourceNotFoundException.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends Exception {
 
+    private static final long serialVersionUID = 1L;
+
     public ResourceNotFoundException(String message){
         super(message);
     }
