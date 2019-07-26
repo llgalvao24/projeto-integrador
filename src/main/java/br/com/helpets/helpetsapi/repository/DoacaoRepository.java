27927d5d55122d04dbbd48c58e7f6d7fbed9f434
FillipiSilva <fillipi.silva.fs@gmail.com>
2019-07-26 08:22:37 -0300

    Reset do projeto para commit anterior

--- src/main/java/br/com/helpets/helpetsapi/repository/DoacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.helpets.helpetsapi.repository;
-
-import br.com.helpets.helpetsapi.model.Doacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DoacaoRepository extends JpaRepository<Doacao, Long> {
-}
