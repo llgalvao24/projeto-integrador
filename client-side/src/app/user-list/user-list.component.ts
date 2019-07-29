b7e4e98b5cd931a62094ac6718022bb7d89b0f64
Luiz G. Galvão <llgalvao24@gmail.com>
2019-07-29 10:07:16 -0300

    Client-side adicionado

---
+++ client-side/src/app/user-list/user-list.component.ts
@@ -0,0 +1,39 @@
+import {Component, OnInit} from "@angular/core";
+import {Observable} from "rxjs";
+import {User} from "../user";
+import {UserService} from "../user.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: "app-user-list",
+  templateUrl: "./user-list.component.html",
+  styleUrls: ["./user-list.component.css"]
+})
+export class UserListComponent implements OnInit {
+  users: Observable<User[]>;
+
+  constructor(private userService: UserService,
+              private router: Router) {}
+
+  ngOnInit() {
+    this.reloadData();
+  }
+
+  reloadData() {
+    this.users = this.userService.getUsersList();
+  }
+
+  deleteUser(id: number) {
+    this.userService.deleteUser(id)
+      .subscribe(
+        data => {
+          console.log(data);
+          this.reloadData();
+        },
+        error => console.log(error));
+  }
+
+  userDetails(id: number){
+    this.router.navigate(['details', id]);
+  }
+}
